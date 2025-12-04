7da10ffb0b7670d65f75684750e6d272c14fff31
3wg3n33 <evgen.pirs@gmail.com>
2025-12-04 20:55:27 +0100

    Update send.php

--- send.php
+++ send.php
@@ -1,1 +1,21 @@
+<?php
+if($_POST) {
+    $ime = $_POST['name'];
+    $tel = $_POST['phone'];
+    $sporocilo = $_POST['message'];
+
+    $to = "TVOJEMAIL@GMAIL.COM";   // <- evgen.pirs@gmail.com
+    $zadeva = "Nova prijava z obrazca";
+
+    $tekst = "Ime: $ime\n";
+    $tekst .= "Telefon: $tel\n";
+    $tekst .= "Sporočilo: $sporocilo\n";
+
+    $headers = "From: noreply@tvoja-domena.si";
+
+    mail($to, $zadeva, $tekst, $headers);
+
+    header("Location: uspeh.html");
+}
+?>
 
